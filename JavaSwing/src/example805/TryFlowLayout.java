8efbdc8915b2c24e0a53d7013a0602053181f64c
shamimsjava <shamimsjava@gmail.com>
2017-10-23 23:14:39 +0600

    Advance java practice

---
+++ JavaSwing/src/example805/TryFlowLayout.java
@@ -0,0 +1,11 @@
+package example805;
+
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+
+public class TryFlowLayout {
+    static JFrame aWindow = new JFrame("This is a Flow Layout");
+    public static void main(String[] args) {
+        Toolkit theKit = aWindow.getToolkit();
+    }
+}
