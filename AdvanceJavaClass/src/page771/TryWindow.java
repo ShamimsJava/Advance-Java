3f63919e029ebb0c8e2633a8e02c92a2155b6ddf
shamimsjava <shamimsjava@gmail.com>
2017-10-25 21:31:24 +0600

    Practice

---
+++ AdvanceJavaClass/src/page771/TryWindow.java
@@ -0,0 +1,12 @@
+package page771;
+
+import javax.swing.JFrame;
+
+public class TryWindow {
+    static JFrame aWindow = new JFrame("This is title");
+    public static void main(String[] args) {
+        aWindow.setBounds(50,100,400,150);
+        aWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        aWindow.setVisible(true);
+    }
+}
