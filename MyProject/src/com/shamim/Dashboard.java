8632ef7eb695cd89e21a5d6b5a075b35e5bcb8a7
ShamimsJava <shamimsjava@gmail.com>
2017-10-31 08:53:37 +0600

    practice

--- MyProject/src/com/shamim/Dashboard.java
+++ MyProject/src/com/shamim/Dashboard.java
@@ -55,6 +55,7 @@ public class Dashboard extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new Dashboard().setVisible(true);
             }
